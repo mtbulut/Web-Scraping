1455c4baaaa40f541a029e845e4044a84cb183ab
Memet Bulut <mbulut@tamusa.edu>
2020-03-02 15:33:09 -0600

    working on app.py file

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ import scrape_mars, pymongo   # Mongo will be used in python.
 app = Flask(__name__)
 
 CONN = "mongodb://localhost:27017"
+client = pymongo.MongoClient(CONN)
+db = client.marsinfo_db
+
+@app.route("/")
+def index():
+    mars_data = db.marsdata.find_one()
+    mars_data = scrape_mars.scrape()
+    return render_template("index.html", mars = mars_data)
+
+@app.route("/scrape")
+def scrape():
+    mars_data = scrape_mars.scrape() 
+    print(mars_data)
+    db.marsdata.drop()
+    db.marsdata.insert_one(mars_data)
+    
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
